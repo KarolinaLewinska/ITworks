2e7e7d1e5266e419927ec442268f56f38b06442e
Justyna Gapys <justynagapys0310@gmail.com>
2021-04-04 23:52:30 +0200

    itSchoolCourses - 04.04

---
+++ scrapping_scripts/itschool_scripping_scripts/php.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+from requests import get
+import sqlite3
+from sys import argv
+
+URL = 'https://itschool.pl/szkolenia/web-development/'
+db = sqlite3.connect('courses.db')
+cursor = db.cursor()
+
+page = get (URL)
+bs = BeautifulSoup(page.content, 'html.parser')
+courses = bs.find_all('a', class_='training-box')
+for course in courses:
+    course_title = course.find('h4', class_='name').get_text()
+    course_url = bs.find('a', class_='training-box')['href']
+    cursor.execute ('INSERT INTO courses VALUES (?, ?)', (course_title, course_url))
+db.commit()
+
+db.close()
