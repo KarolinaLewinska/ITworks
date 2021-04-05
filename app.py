25aee1321a2644fe2e837d157aececcf13d07a87
Justyna Gapys <justynagapys0310@gmail.com>
2021-04-05 10:00:33 +0200

    filesRenameAndFlaskStructure - 05.04
    Co-authored-by: Karolina Lewińska <lewinskak95@gmail.com>

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/offers', methods=['GET', 'POST'] )
+def result():       
+    return render_template('offers.xhtml')    
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('error.html')    
+
 if __name__ == "__main__":
     app.run()
