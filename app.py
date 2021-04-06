da638cf0e5d167a48096386154ec88c71a095d2f
Justyna Gapys <justynagapys0310@gmail.com>
2021-04-06 16:07:59 +0200

    workingSearch - commit 06.04
    Co-authored-by: Karolina Lewińska <lewinskak95@gmail.com>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -9,8 +9,74 @@ def home():
 
 @app.route('/browser', methods=['GET', 'POST'] )
 def browser():
-        # miejsce na kod do bazy
-    return render_template('browser.html')  
+    if request.method == "POST":
+        if 'java' in request.form:
+            conn = sqlite3.connect('courses.db')
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            cur.execute("SELECT * from courses WHERE course_title LIKE '%java%'")
+            conn.commit()
+            rows = cur.fetchall()
+            return render_template('browser.html', rows=rows)
+        elif 'html' in request.form:
+            conn = sqlite3.connect('courses.db')
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            cur.execute("SELECT * from courses WHERE course_title LIKE '%html%'")
+            conn.commit()
+            rows = cur.fetchall()
+            return render_template('browser.html', rows=rows)
+        elif 'csharp' in request.form:
+            conn = sqlite3.connect('courses.db')
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            cur.execute("SELECT * from courses WHERE course_title LIKE '%c#%'") 
+            conn.commit()
+            rows = cur.fetchall()
+            return render_template('browser.html', rows=rows)
+        elif 'javascript' in request.form:
+            conn = sqlite3.connect('courses.db')
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            cur.execute("SELECT * from courses WHERE course_title LIKE '%javascript%'")
+            conn.commit()
+            rows = cur.fetchall()
+            return render_template('browser.html', rows=rows)
+        elif 'php' in request.form:
+            conn = sqlite3.connect('courses.db')
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            cur.execute("SELECT * from courses WHERE course_title LIKE '%php%'")
+            conn.commit()
+            rows = cur.fetchall()
+            return render_template('browser.html', rows=rows)
+        elif 'python' in request.form:
+            conn = sqlite3.connect('courses.db')
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            cur.execute("SELECT * from courses WHERE course_title LIKE '%python%'")
+            conn.commit()
+            rows = cur.fetchall()
+            return render_template('browser.html', rows=rows)
+        elif 'sql' in request.form:
+            conn = sqlite3.connect('courses.db')
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            cur.execute("SELECT * from courses WHERE course_title LIKE '%sql%'") 
+            conn.commit()
+            rows = cur.fetchall()
+            return render_template('browser.html', rows=rows)
+        else:
+            conn = sqlite3.connect('courses.db')
+            conn.row_factory = sqlite3.Row
+            cur = conn.cursor()
+            search = request.form['search']
+            cur.execute("SELECT * from courses")
+            conn.commit()
+            rows = cur.fetchall(); 
+            return render_template('browser.html', rows=rows)
+              
+    return render_template('browser.html')
 
 @app.errorhandler(500) 
 def internal_error(error):
