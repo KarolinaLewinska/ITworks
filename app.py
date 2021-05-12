94390a7c5cd15bcb42557d3f31f2987332b6191d
Justyna Gapys <justynagapys0310@gmail.com>
2021-05-12 10:48:47 +0200

    designChanges-12.05

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash, redirect
 import sqlite3
-from flask_paginate import Pagination, get_page_args
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -47,7 +46,6 @@ def browser():
                     flash("Liczba dostępnych kursów: " + str(len(rows)))
                 if not rows:
                     flash("Brak kursów w bazie! Wpisz w wyszukiwarkę inne zagadnienie.")
-
                 conn.close()
                 return render_template('browser.html', rows=rows)
             except OperationalError:
@@ -55,83 +53,60 @@ def browser():
 
 @app.route('/search-java', methods=['GET','POST'])
 def search_java():
-    def get_courses(offset=0, per_page=10):
-        return rows[offset: offset + per_page]
     try:
         conn = sqlite3.connect('courses.db')
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        query = "SELECT * from courses WHERE course_title LIKE '%java%' ORDER BY course_title"
+        query = "SELECT * from courses WHERE course_title LIKE '% java %' ORDER BY course_title"
         cur.execute(query)
         conn.commit()
         rows = cur.fetchall()
         rows_number = len(rows)
         if rows:
             flash("Liczba dostępnych kursów: " + str(len(rows)))
-        page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        total = len(rows)
-        pagination_courses = get_courses(offset=offset, per_page=per_page)
-        pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         conn.close()
-        return render_template('categories_search_results.html', rows=pagination_courses, 
-            page=page, per_page=per_page,pagination=pagination)
+        return render_template('categories_search_results.html', rows=rows)
     except OperationalError:
             flash("Wystąpił błąd podczas próby połączenia się z bazą")
 
 @app.route('/search-html', methods=['GET','POST'])
 def search_html():
-    def get_courses(offset=0, per_page=10):
-        return rows[offset: offset + per_page]
     try:
         conn = sqlite3.connect('courses.db')
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        query = "SELECT * from courses WHERE course_title LIKE '%html%' ORDER BY course_title"
+        query = "SELECT * from courses WHERE course_title LIKE '% html %' ORDER BY course_title"
         cur.execute(query)
         conn.commit()
         rows = cur.fetchall()
         rows_number = len(rows)
         if rows:
             flash("Liczba dostępnych kursów: " + str(len(rows)))
-        page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        total = len(rows)
-        pagination_courses = get_courses(offset=offset, per_page=per_page)
-        pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         conn.close()
-        return render_template('categories_search_results.html', rows=pagination_courses, 
-        page=page, per_page=per_page, pagination=pagination)
+        return render_template('categories_search_results.html', rows=rows)
     except OperationalError:
         flash("Wystąpił błąd podczas próby połączenia się z bazą")
 
 @app.route('/search-csharp', methods=['GET','POST'])
 def search_csharp():
-    def get_courses(offset=0, per_page=10):
-        return rows[offset: offset + per_page]
     try:
         conn = sqlite3.connect('courses.db')
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        query = "SELECT * from courses WHERE course_title LIKE '%c#%' ORDER BY course_title"
+        query = "SELECT * from courses WHERE course_title LIKE '% c# %' ORDER BY course_title"
         cur.execute(query)
         conn.commit()
         rows = cur.fetchall()
         rows_number = len(rows)
         if rows:
             flash("Liczba dostępnych kursów: " + str(len(rows)))
-        page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        total = len(rows)
-        pagination_courses = get_courses(offset=offset, per_page=per_page)
-        pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         conn.close()
-        return render_template('categories_search_results.html', rows=pagination_courses, 
-            page=page, per_page=per_page, pagination=pagination)
+        return render_template('categories_search_results.html', rows=rows)
     except OperationalError:
         flash("Wystąpił błąd podczas próby połączenia się z bazą")
 
 @app.route('/search-c', methods=['GET','POST'])
 def search_c():
-    def get_courses(offset=0, per_page=10):
-        return rows[offset: offset + per_page]
     try:
         conn = sqlite3.connect('courses.db')
         conn.row_factory = sqlite3.Row
@@ -143,95 +118,67 @@ def search_c():
         rows_number = len(rows)
         if rows:
             flash("Liczba dostępnych kursów: " + str(len(rows)))
-        page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        total = len(rows)
-        pagination_courses = get_courses(offset=offset, per_page=per_page)
-        pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         conn.close()
-        return render_template('categories_search_results.html', rows=pagination_courses, 
-            page=page, per_page=per_page, pagination=pagination)
+        return render_template('categories_search_results.html', rows=rows)
     except OperationalError:
         flash("Wystąpił błąd podczas próby połączenia się z bazą")
 
 @app.route('/search-cplusplus', methods=['GET','POST'])
 def search_cplusplus():
-    def get_courses(offset=0, per_page=10):
-        return rows[offset: offset + per_page]
     try:
         conn = sqlite3.connect('courses.db')
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        query = "SELECT * from courses WHERE course_title LIKE '%c++%' ORDER BY course_title"
+        query = "SELECT * from courses WHERE course_title LIKE '% c++ %' ORDER BY course_title"
         cur.execute(query)
         conn.commit()
         rows = cur.fetchall()
         rows_number = len(rows)
         if rows:
             flash("Liczba dostępnych kursów: " + str(len(rows)))
-        page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        total = len(rows)
-        pagination_courses = get_courses(offset=offset, per_page=per_page)
-        pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         conn.close()
-        return render_template('categories_search_results.html', rows=pagination_courses, 
-            page=page, per_page=per_page, pagination=pagination)
+        return render_template('categories_search_results.html', rows=rows)
     except OperationalError:
         flash("Wystąpił błąd podczas próby połączenia się z bazą")
 
 @app.route('/search-javascript', methods=['GET','POST'])
 def search_javascript():
-    def get_courses(offset=0, per_page=10):
-        return rows[offset: offset + per_page]
     try:
         conn = sqlite3.connect('courses.db')
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        query = "SELECT * from courses WHERE course_title LIKE '%javascript%' ORDER BY course_title"
+        query = "SELECT * from courses WHERE course_title LIKE '% javascript %' ORDER BY course_title"
         cur.execute(query)
         conn.commit()
         rows = cur.fetchall()
         rows_number = len(rows)
         if rows:
             flash("Liczba dostępnych kursów: " + str(len(rows)))
-        page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        total = len(rows)
-        pagination_courses = get_courses(offset=offset, per_page=per_page)
-        pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         conn.close()
-        return render_template('categories_search_results.html', rows=pagination_courses, 
-            page=page, per_page=per_page, pagination=pagination)
+        return render_template('categories_search_results.html', rows=rows)
     except OperationalError:
         flash("Wystąpił błąd podczas próby połączenia się z bazą")
 
 @app.route('/search-php', methods=['GET','POST'])
 def search_php():
-    def get_courses(offset=0, per_page=10):
-        return rows[offset: offset + per_page]
     try:
         conn = sqlite3.connect('courses.db')
         conn.row_factory = sqlite3.Row
         cur = conn.cursor()
-        query = "SELECT * from courses WHERE course_title LIKE '%php%' ORDER BY course_title"
+        query = "SELECT * from courses WHERE course_title LIKE '% php %' ORDER BY course_title"
         cur.execute(query)
         conn.commit()
         rows = cur.fetchall()
         rows_number = len(rows)
         if rows:
             flash("Liczba dostępnych kursów: " + str(len(rows)))
-        page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        total = len(rows)
-        pagination_courses = get_courses(offset=offset, per_page=per_page)
-        pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         conn.close()
-        return render_template('categories_search_results.html', rows=pagination_courses, 
-            page=page, per_page=per_page, pagination=pagination)
+        return render_template('categories_search_results.html', rows=rows)
     except OperationalError:
         flash("Wystąpił błąd podczas próby połączenia się z bazą")
 
 @app.route('/search-python', methods=['GET','POST'])
 def search_python():
-    def get_courses(offset=0, per_page=10):
-        return rows[offset: offset + per_page]
     try:
         conn = sqlite3.connect('courses.db')
         conn.row_factory = sqlite3.Row
@@ -243,20 +190,13 @@ def search_python():
         rows_number = len(rows)
         if rows:
             flash("Liczba dostępnych kursów: " + str(len(rows)))
-        page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        total = len(rows)
-        pagination_courses = get_courses(offset=offset, per_page=per_page)
-        pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         conn.close()
-        return render_template('categories_search_results.html', rows=pagination_courses, 
-            page=page, per_page=per_page, pagination=pagination)
+        return render_template('categories_search_results.html', rows=rows)
     except OperationalError:
         flash("Wystąpił błąd podczas próby połączenia się z bazą")
 
 @app.route('/search-sql', methods=['GET','POST'])
 def search_sql():
-    def get_courses(offset=0, per_page=10):
-        return rows[offset: offset + per_page]
     try:
         conn = sqlite3.connect('courses.db')
         conn.row_factory = sqlite3.Row
@@ -268,13 +208,8 @@ def search_sql():
         rows_number = len(rows)
         if rows:
             flash("Liczba dostępnych kursów: " + str(len(rows)))
-        page, per_page, offset = get_page_args(page_parameter='page', per_page_parameter='per_page')
-        total = len(rows)
-        pagination_courses = get_courses(offset=offset, per_page=per_page)
-        pagination = Pagination(page=page, per_page=per_page, total=total, css_framework='bootstrap4')
         conn.close()
-        return render_template('categories_search_results.html', rows=pagination_courses, 
-            page=page, per_page=per_page, pagination=pagination)
+        return render_template('categories_search_results.html', rows=rows)
     except OperationalError:
         flash("Wystąpił błąd podczas próby połączenia się z bazą")
 
